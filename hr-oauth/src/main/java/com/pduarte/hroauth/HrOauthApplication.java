4458e70a406c8625da1c6706bca0edee77b87231
psmatos <psmatos@minsait.com>
2021-02-26 14:39:13 +0100

    Project hr-oauth created

--- hr-oauth/src/main/java/com/pduarte/hroauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/com/pduarte/hroauth/HrOauthApplication.java
@@ -1,4 +1,4 @@
-package com.pduarte.hr;
+package com.pduarte.hroauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- hr-oauth/src/test/java/com/pduarte/hroauth/HrOauthApplicationTests.java
+++ hr-oauth/src/test/java/com/pduarte/hroauth/HrOauthApplicationTests.java
@@ -1,4 +1,4 @@
-package com.pduarte.hr;
+package com.pduarte.hroauth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
