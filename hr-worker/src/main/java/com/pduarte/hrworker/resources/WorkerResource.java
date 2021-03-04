b6cb99e7015d146340a0fa408d5f0ccc46c85ae8
psmatos <psmatos@minsait.com>
2021-03-04 10:05:19 +0100

    db config server

--- hr-worker/src/main/java/com/pduarte/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/pduarte/hrworker/resources/WorkerResource.java
@@ -24,8 +24,6 @@ public class WorkerResource {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 	
-	@Value("${test.config}")
-	private String testConfig;
 	
 	@Autowired
 	private Environment env;
@@ -35,7 +33,7 @@ public class WorkerResource {
 	
 	@GetMapping(value="/configs")
 	public ResponseEntity<Void> getConfigs(){
-		logger.info(testConfig);
+		//logger.info(testConfig);
 		return ResponseEntity.noContent().build();
 	}
 	
@@ -50,12 +48,12 @@ public class WorkerResource {
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
 		
 		
-		try {
-			Thread.sleep(3000L);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		try {
+//			Thread.sleep(3000L);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
